307121b9aefa493406c8f402bad5b0a671fc324a
unknown <tpc3yw@virginia.edu>
2020-07-11 21:44:17 -0400

    ready to deploy

---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('', function(req, res){
+    res.render('diagram', {passedData: data});
+});
+app.get('/info', function(req, res){
+    res.render('dmas_info')
+});
+app.get('/downloadAll', function(req, res){
+    res.download('./AllBudgets.xls', 'SankeyDataBudget.xls');
+});
+app.get('/downloadBreakdown', function(req, res){
+    res.download('./dmas_expend_3ways.xls', 'Age_and_County_Breakdown.xls');
+});
+app.get('/ageBreakdown', function(req, res){
+    res.render('breakdown_age');
+});
+app.get('/countyBreakdown', function(req, res){
+    res.render('breakdown_county');
+});
+
+app.listen(3000);
+
+
+// const Client = require('pg').Client
+// const client = new Client({
+//     user: "tpc3yw@va-energy2",
+//     password: "3fuzzyDice",
+//     host: "va-energy2.postgres.database.azure.com",
+//     port: "5432",
+//     database: "postgres" 
+// })
+
+const Client = require('pg').Client
+const client = new Client({
+    user: "postgres", 
+    password: "password",
+    host: "localhost",
+    port: "5432",
+    database: "testdata"
+})
+
+var data = [];
+client.connect()
+.then(() => console.log("Connected Succesfully"))
+.then(() => client.query('SELECT "From", "To", CAST("Value" AS DOUBLE PRECISION), "Webpage" FROM test'))
+.then(results => {for(var i = 0; i < results.rowCount; i++){
+    var row = results.rows[i];
+    var from = row.From;
+    var to = row.To;
+    var value = parseInt(row.Value);
+    var webpage = row.Webpage
+    if(i == 0){
+        from = "Assesments for Special Services";
+        to = "Total Budget";
+        value = 2360778362;
+        url = ""
+    }
+    data.push([from, to, value, webpage]);
+}})
+.catch(e => console.log(e))
+.finally(() => client.end())
